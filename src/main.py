73791ae94f2db62392b7504ca989b804b5721dc5
David <dmcfarland8031@gmail.com>
2024-03-27 21:09:26 -0600

    Source files made

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+
+class Project:
+    def __init__(self) -> None:
+        self.project_name = str
+        self.cpp_resource_loc = ""
+        self.python_resource_loc = ""
+
+
+    
